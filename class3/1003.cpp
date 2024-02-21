2d547d0edbc2b9e11f8453d53c3037898296778a
DAECHUNG YOO <dcyoo777@gmail.com>
2024-02-21 16:53:51 +0900

    1003

---
+++ class3/1003.cpp
@@ -0,0 +1,33 @@
+#include "class3.h"
+#include "iostream"
+
+using namespace std;
+
+int b1003() {
+
+    int t;
+    int fill = 1;
+    int fibos[41][2];
+    int n;
+
+    fibos[0][0] = 1;
+    fibos[0][1] = 0;
+    fibos[1][0] = 0;
+    fibos[1][1] = 1;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        cin >> n;
+        if (n > fill) {
+            for (int j = fill + 1; j <= n; j++) {
+                fibos[j][0] = fibos[j - 1][0] + fibos[j - 2][0];
+                fibos[j][1] = fibos[j - 1][1] + fibos[j - 2][1];
+            }
+        }
+        cout << fibos[n][0] << " " << fibos[n][1] << "\n";
+
+    }
+
+    return 0;
+}
---
+++ class3/class3.h
@@ -0,0 +1,10 @@
+//
+// Created by 유대청 on 2/21/24.
+//
+
+#ifndef BJ_CLASS3_H
+#define BJ_CLASS3_H
+
+int b1003();
+
+#endif //BJ_CLASS3_H
