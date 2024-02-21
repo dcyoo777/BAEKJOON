2f45e34a2578c7dafd064e7ae3e80f7335f74709
DAECHUNG YOO <dcyoo777@gmail.com>
2024-02-21 18:48:40 +0900

    1074

--- class3/1074.cpp
+++ class3/1074.cpp
@@ -1,5 +1,6 @@
 #include "class3.h"
 #include "iostream"
+#include <cmath>
 
 using namespace std;
 
@@ -7,6 +8,20 @@ int b1074() {
 
     ios::sync_with_stdio(false);
     cin.tie(0);
+
+    long n, r, c;
+    long result = 0;
+
+    cin >> n >> r >> c;
+
+    for (long i = n - 1; i >= 0; i--) {
+        long a = (long)pow(2, i);
+        result += a * a * (r / a * 2 + c / a);
+        r %= a;
+        c %= a;
+    }
+
+    cout << result;
 
     return 0;
 }
